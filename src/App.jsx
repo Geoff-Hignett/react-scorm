4a986831e1d738009a8f90aa9d31b8631ed4f38a
Geoff Hignett <geoffhig@hotmail.com>
2023-06-19 19:50:23 +0100

    react-scorm-provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import { Introduction } from "./pages/Introduction";
 import { Section1 } from "./pages/Section1";
 import { Summary } from "./pages/Summary";
 import { AppProvider } from "./AppContext";
+import { Learner } from "./scorm/Learner";
+import ScormProvider from "./scorm/index";
+import ApiStatus from "./scorm/ApiStatus";
 
 import "./index.css";
 
 function App() {
     return (
         <>
-            <AppProvider>
-                <Routes>
-                    <Route path="/" element={<Introduction />} />
-                    <Route path="/section1" element={<Section1 />} />
-                    <Route path="/summary" element={<Summary />} />
-                </Routes>
-            </AppProvider>
+            <ScormProvider>
+                <AppProvider>
+                    <Routes>
+                        <Route path="/" element={<Introduction />} />
+                        <Route path="/section1" element={<Section1 />} />
+                        <Route path="/summary" element={<Summary />} />
+                    </Routes>
+                    <Learner />
+                    <ApiStatus />
+                </AppProvider>
+            </ScormProvider>
         </>
     );
 }
